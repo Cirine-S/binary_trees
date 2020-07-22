035fc609a95ed344d0675159e023233e192498d3
usfbelhadj <usf.belhadj@gmail.com>
2020-07-22 15:12:19 +0100

    Delete

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -2,18 +2,18 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - function
+ * binary_tree_delete - function
  *
  * Return: Always 0 (Success)
  */
- void binary_tree_delete(binary_tree_t *tree)
- {
-    if (tree == NULL)
-        return;
-  
-    /* first delete both subtrees recursively*/
-    binary_tree_delete(tree->left); 
-    binary_tree_delete(tree->right); 
-    
-    free(tree); 
- }
+void binary_tree_delete(binary_tree_t *tree)
+{
+if (tree == NULL)
+return;
+
+/* first delete both subtrees recursively*/
+binary_tree_delete(tree->left);
+binary_tree_delete(tree->right);
+
+free(tree);
+}
