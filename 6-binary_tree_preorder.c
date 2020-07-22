010fbadd412b2abdc38b57b4630305fd7289f0ef
usfbelhadj <usf.belhadj@gmail.com>
2020-07-22 15:23:26 +0100

    fix style

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -2,16 +2,16 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - function
+ * binary_tree_preorder - function
  *@func: function to call
  * @tree: input tree
  * Return: Always 0 (Success)
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (!tree || !func)
-        return;
-    func(tree->n);
-    binary_tree_preorder(tree->left, func);
-    binary_tree_preorder(tree->right, func);
+if (!tree || !func)
+return;
+func(tree->n);
+binary_tree_preorder(tree->left, func);
+binary_tree_preorder(tree->right, func);
 }
