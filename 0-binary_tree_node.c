a3a90dbcd9110cfadc4a07de3c75482da6c60574
usfbelhadj <usf.belhadj@gmail.com>
2020-07-22 15:03:55 +0100

    Fiix

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -3,20 +3,21 @@
 
 /**
  * binary_tree_node - function
- *
+ * @parent: root
+ * @value: int
  * Return: Always 0 (Success)
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
-    
-    new_node = malloc(sizeof(binary_tree_t));
-    if (!new_node)
-        return NULL;
+binary_tree_t *new_node;
 
-    new_node->n = value;
-    new_node->parent = parent;
-    new_node->left = NULL;
-    new_node->right = NULL;
-    return new_node;
+new_node = malloc(sizeof(binary_tree_t));
+if (!new_node)
+return (NULL);
+
+new_node->n = value;
+new_node->parent = parent;
+new_node->left = NULL;
+new_node->right = NULL;
+return (new_node);
 }
