18460f4e14a584f6495c387acb916a690442fd29
usfbelhadj <usf.belhadj@gmail.com>
2020-07-22 15:06:42 +0100

    Trry

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -2,29 +2,30 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - function
- *
+ * binary_tree_insert_left- function
+ * @parent: root
+ * @value: int
  * Return: Always 0 (Success)
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+binary_tree_t *new_node;
 
-    if (parent == NULL)
-        return NULL;
-    new_node = malloc(sizeof(binary_tree_t));
-    if (!new_node)
-        return NULL;
+if (parent == NULL)
+return (NULL);
+new_node = malloc(sizeof(binary_tree_t));
+if (!new_node)
+return (NULL);
 
-    if (parent->left)
-    {   
-        new_node->left = parent->left;
-        new_node->left->parent = new_node;
-    }
-            
-    new_node->n = value;
-    parent->left = new_node;
-    new_node->parent = parent;
+if (parent->left)
+{
+new_node->left = parent->left;
+new_node->left->parent = new_node;
+}
 
-    return new_node;
+new_node->n = value;
+parent->left = new_node;
+new_node->parent = parent;
+
+return (new_node);
 }
