eb407149105f1bf4e4addd69ccae0d92816c0ae8
usfbelhadj <usf.belhadj@gmail.com>
2020-07-22 15:15:29 +0100

    Fixed Style

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -3,14 +3,14 @@
 
 /**
  * binary_tree_node - function
- *
+ * @node: pointer to input node
  * Return: Always 0 (Success)
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    if (node == NULL)
-        return 0;
-    if (!node->right && !node->left)
-        return 1;
-    return 0;
+if (node == NULL)
+return (0);
+if (!node->right && !node->left)
+return (1);
+return (0);
 }
