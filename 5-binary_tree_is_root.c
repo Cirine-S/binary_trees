d502884964c2ec39dc54b120ce403f85b97cdc3c
usfbelhadj <usf.belhadj@gmail.com>
2020-07-22 15:18:32 +0100

    Style

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -2,15 +2,15 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - function
- *
+ * binary_tree_is_root - function
+ *@node: pointer to input node
  * Return: Always 0 (Success)
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-    if (node == NULL)
-        return 0;
-    if (!node->parent)
-        return 1;
-    return 0;
+if (node == NULL)
+return (0);
+if (!node->parent)
+return (1);
+return (0);
 }
