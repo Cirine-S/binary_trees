7af252f7eb7c9f8bd107d7f4036763ce43e80748
usfbelhadj <usf.belhadj@gmail.com>
2020-07-22 15:33:39 +0100

    Test

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -2,8 +2,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_inorder- function
- *@func: function to call
+ * binary_tree_inorder - function
+ * @func: function to call
  * @tree: input tree
  * Return: Always 0 (Success)
  */
@@ -11,7 +11,7 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 if (!tree || !func)
 return;
-binary_tree_preorder(tree->left, func);
+binary_tree_inorder(tree->left, func);
 func(tree->n);
-binary_tree_preorder(tree->right, func);
+binary_tree_inorder(tree->right, func);
 }
