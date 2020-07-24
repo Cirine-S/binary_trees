19fbf8ae9d3f7486412e9f5709c8c84645b3cc99
cirine <1410@holbertonschool.com>
2020-07-24 01:41:42 +0100

    height of the tree

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Function
+ * @tree: input tree
+ * Return: Height of binary tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t lefth, righth;
+
+	if (!tree || (!tree->left && !tree->right))
+		return (0);
+	lefth = binary_tree_height(tree->left);
+	righth = binary_tree_height(tree->right);
+	if (lefth >= righth)
+		return (lefth + 1);
+	else
+		return (righth + 1);
+}
