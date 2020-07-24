909cab9abaf8bd0b03c098a9c976d0f4f03c05e2
usfbelhadj <usf.belhadj@gmail.com>
2020-07-24 11:52:14 +0100

    Fiix style

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,17 +9,17 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
-    if (parent == NULL)
-        return NULL;
-    new_node = binary_tree_node(parent, value);
-    if (!new_node)
-        return NULL;
-    if (parent->right)
-    {   
-        new_node->right = parent->right;
-        new_node->right->parent = new_node;
-    }         
-    parent->right = new_node;
-    return new_node;
+binary_tree_t *new_node;
+if (parent == NULL)
+return (NULL);
+new_node = binary_tree_node(parent, value);
+if (!new_node)
+return (NULL);
+if (parent->right)
+{
+new_node->right = parent->right;
+new_node->right->parent = new_node;
+}
+parent->right = new_node;
+return (new_node);
 }
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,14 +7,14 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t lefth, righth;
+size_t lefth, righth;
 
-	if (!tree || (!tree->left && !tree->right))
-		return (0);
-	lefth = binary_tree_height(tree->left);
-	righth = binary_tree_height(tree->right);
-	if (lefth >= righth)
-		return lefth + 1;
-	else
-		return righth + 1;
+if (!tree || (!tree->left && !tree->right))
+return (0);
+lefth = binary_tree_height(tree->left);
+righth = binary_tree_height(tree->right);
+if (lefth >= righth)
+return (lefth + 1);
+else
+return (righth + 1);
 }
