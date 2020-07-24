c6da89703cd2af849b6c520ec71c6fef19b8062a
cirine <1410@holbertonschool.com>
2020-07-24 02:21:27 +0100

    update

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -8,9 +8,9 @@
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-if (node == NULL)
-return (0);
-if (!node->parent)
-return (1);
-return (0);
+    if (node == NULL)
+        return 0;
+    if (!node->right && !node->left)
+        return 1;
+    return 0;
 }
