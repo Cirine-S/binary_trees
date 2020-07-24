8a08a728cade28c457966cb29768142ca596a0bc
cirine <1410@holbertonschool.com>
2020-07-24 01:17:05 +0100

    correction

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,7 +13,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
     if (parent == NULL)
         return NULL;
-    new_node = malloc(sizeof(binary_tree_t));
+    new_node = binary_tree_node(parent, value);
     if (!new_node)
         return NULL;
 
@@ -23,8 +23,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
         new_node->left = parent->left;
     }
     parent->left = new_node;
-    new_node->n = value;
-    new_node->parent = parent;
 
     return new_node;
 }
