1d44ce2f3798bd3e01cf82e16305bbff90dcdaa1
usfbelhadj <usf.belhadj@gmail.com>
2020-07-24 11:48:31 +0100

    Fixx

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,20 +9,20 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+binary_tree_t *new_node;
 
-    if (parent == NULL)
-        return NULL;
-    new_node = binary_tree_node(parent, value);
-    if (!new_node)
-        return NULL;
+if (parent == NULL)
+return (NULL);
+new_node = binary_tree_node(parent, value);
+if (!new_node)
+return (NULL);
 
-    if (parent->left)
-    {   
-        new_node->left = parent->left;
-        new_node->left->parent = new_node;
-    }
-    parent->left = new_node;
+if (parent->left)
+{
+new_node->left = parent->left;
+new_node->left->parent = new_node;
+}
+parent->left = new_node;
 
-    return new_node;
+return (new_node);
 }
