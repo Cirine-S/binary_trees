ecb5d45623cac9f95cb5ac7227d847053d0485a2
cirine <1410@holbertonschool.com>
2020-07-24 01:05:46 +0100

    correction

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -5,26 +5,26 @@
  * binary_tree_insert_left- function
  * @parent: root
  * @value: int
- * Return: Always 0 (Success)
+ * Return: new left node
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-binary_tree_t *new_node;
+    binary_tree_t *new_node;
 
-if (parent == NULL)
-return (NULL);
-new_node = malloc(sizeof(binary_tree_t));
-if (!new_node)
-return (NULL);
+    if (parent == NULL)
+        return NULL;
+    new_node = malloc(sizeof(binary_tree_t));
+    if (!new_node)
+        return NULL;
 
-if (parent->left)
-{
-new_node->left = parent->left;
-new_node->left->parent = new_node;
-}
+    if (parent->left)
+    {   
+        parent->left->parent = new_node;
+        new_node->left = parent->left;
+    }
+    parent->left = new_node;
+    new_node->n = value;
+    new_node->parent = parent;
 
-new_node->n = value;
-new_node->parent = parent;
-
-return (new_node);
+    return new_node;
 }
