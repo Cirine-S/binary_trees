cfcd80bbe3f2cec5fbc1b92e61920a97a5455d7e
cirine <1410@holbertonschool.com>
2020-07-24 02:02:01 +0100

    tree balance red output checks

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,20 +8,20 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t lh, rh;
+	size_t lefth, righth;
 
 	if (!tree || (!tree->left && !tree->right))
 		return (0);
-	lh = binary_tree_height(tree->left);
-	rh = binary_tree_height(tree->right);
-	if (lh >= rh)
-		return (lh + 1);
+	lefth = binary_tree_height(tree->left);
+	righth = binary_tree_height(tree->right);
+	if (lefth >= righth)
+		return (lefth + 1);
 	else
-		return (rh + 1);
+		return (righth + 1);
 }
 
 /**
- * binary_tree_balance - Function
+ * binary_tree_balance(- Function
  * @tree: input tree
  * Return: the balance factor
  */
@@ -33,5 +33,5 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 	lefth = binary_tree_height(tree->left);
 	righth = binary_tree_height(tree->right);
-	return lefth - righth;
+	return (lefth - righth);
 }
