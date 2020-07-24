c50b379ab70aa8ebcae25773295ba98228ec2b09
cirine <1410@holbertonschool.com>
2020-07-24 01:34:20 +0100

    correction task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -2,9 +2,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - function
- *
- * Return: Always 0 (Success)
+ * binary_tree_insert_left- function
+ * @parent: root
+ * @value: int
+ * Return: new right node
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
@@ -12,19 +13,14 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
     if (parent == NULL)
         return NULL;
-    new_node = malloc(sizeof(binary_tree_t));
+    new_node = binary_tree_node(parent, value);
     if (!new_node)
         return NULL;
-
     if (parent->right)
     {   
         new_node->right = parent->right;
         new_node->right->parent = new_node;
-    }
-            
-    new_node->n = value;
+    }         
     parent->right = new_node;
-    new_node->parent = parent;
-
     return new_node;
 }
