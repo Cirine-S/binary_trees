d4d5a58da47c3f20a36537e17f4003cc035c2285
cirine <1410@holbertonschool.com>
2020-07-24 01:58:25 +0100

    tree balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -2,7 +2,26 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance(- Function
+ * binary_tree_height - Function
+ * @tree: input tree
+ * Return: Always 0 (Success)
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t lh, rh;
+
+	if (!tree || (!tree->left && !tree->right))
+		return (0);
+	lh = binary_tree_height(tree->left);
+	rh = binary_tree_height(tree->right);
+	if (lh >= rh)
+		return (lh + 1);
+	else
+		return (rh + 1);
+}
+
+/**
+ * binary_tree_balance - Function
  * @tree: input tree
  * Return: the balance factor
  */
@@ -14,5 +33,5 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 	lefth = binary_tree_height(tree->left);
 	righth = binary_tree_height(tree->right);
-	return (lefth - righth);
+	return lefth - righth;
 }
