35c4289645ec63f1a2e5f53fd24c7bca054428ce
cirine <1410@holbertonschool.com>
2020-07-24 02:20:52 +0100

    update

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -4,14 +4,14 @@
 /**
  * binary_tree_is_leaf - function
  * @node: pointer to input node
- * Return: Always 0 (Success)
+ * Return: 0 or 1
  */
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-if (node == NULL)
-return (0);
-if (!node->right && !node->left)
-return (1);
-return (0);
+    if (node == NULL)
+        return 0;
+    if (!node->right && !node->left)
+        return 1;
+    return 0;
 }
